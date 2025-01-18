d30ada0a4100a7ba29491d2a0d3337e9aab68408
PiyushSinha-18 <Piyushsinha412@gmail.com>
2025-01-18 20:42:02 +0530

    Update and Delete api routes are added

--- Routes/Person_routes.js
+++ Routes/Person_routes.js
@@ -7,14 +7,14 @@ const person_Model = require('../Models/Person');
 router.post('/', async (req, res) => {
     try {
         const Person_data = req.body;
-        console.log("Person_data ", Person_data);
+        // console.log("Person_data ", Person_data);
 
         const new_Person = new person_Model(Person_data);
-        console.log("new_Person ", new_Person)
+        // console.log("new_Person ", new_Person)
 
         const response = await new_Person.save();
 
-        console.log("response ", response);
+        // console.log("response ", response);
 
         res.send(response).status(200);
 
@@ -33,5 +33,58 @@ router.get('/', async (req, res) => {
         res.send("Server failed").status(500);
     }
 })
+
+router.get('/profile', async (req, res) => {
+    res.send("Welcome to Person Database");
+})
+
+router.get('/:person_name', async (req, res) => {
+    try {
+        const person_Name = req.params.person_name;
+        const response = await person_Model.find({ fname: person_Name });
+        res.json(response).status(200);
+    } catch (error) {
+        console.log(error);
+        res.send("Server failed").status(500);
+    }
+})
+
+router.put('/:personId', async (req, res) => {
+    try {
+        const new_Data = req.body;
+        const personId = req.params.personId;
+        const response = await person_Model.findByIdAndUpdate(personId, new_Data, {
+            new: true,
+            runValidators: true,
+        });
+        if (!response) {
+            console.log("Person not found")
+            return res.status(404).json({ error: "Person not found" })
+        }
+        res.status(200).json(response);
+
+    } catch (error) {
+        console.log(error);
+        res.send("Server failed").status(500);
+    }
+})
+
+router.delete('/:personId', async (req, res) => {
+    try {
+        const personId = req.params.personId;
+        const response = await person_Model.findByIdAndDelete(personId);
+        if (!response) {
+            console.log("Person not found")
+            return res.status(404).json({ error: "Person not found" })
+        }
+        console.log(response);
+        res.status(200).json("Person data deleted");
+
+    } catch (error) {
+        console.log(error);
+        res.send("Server failed").status(500);
+    }
+})
+
 
 module.exports = router;
